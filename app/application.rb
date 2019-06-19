1e106258c3978d31c5f5b0c563a57aa81d5a3cfa
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-06-19 11:32:06 +0800

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,24 @@
 class Application
 
   def call(env)
-    resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp = Rack::Response.new  #this creates an instance of a Rack::Response object
+
+    num_1 = Kernel.rand(1..20)
+    num_2 = Kernel.rand(1..20)
+    num_3 = Kernel.rand(1..20)
+
+    resp.write "#{num_1}\n"
+    resp.write "#{num_2}\n"
+    resp.write "#{num_3}\n"
+
+    if num_1==num_2 && num_2==num_3
+      resp.write "You Win" #write is basically the 'puts' method for web apps
+    else
+      resp.write "You Lose"
+    end
+
     resp.finish
+
   end
 
 end
